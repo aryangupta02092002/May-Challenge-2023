98e4a3d80070a6314190e77ed2187e9957f6f301
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-05-08 10:35:00 +0530

    May 8 - Bit Magic

---
+++ GFG/BinaryModulo.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+        int modulo(string s,int m)
+        {
+            //code here
+            int tmp = 1, ans = 0;
+            for(int i=s.size()-1; i>=0; i--){
+                ans = tmp * (s[i]-'0') + ans;
+                tmp = (tmp*2) % m;
+                ans = ans%m;
+            }
+            return ans;
+        }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        int m;
+        cin>>s>>m;
+        Solution a;
+        cout<<a.modulo(s,m)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
