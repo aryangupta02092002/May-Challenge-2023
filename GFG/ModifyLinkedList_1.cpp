abaec7fbe2353c0aa1e8f0b4108c0fe4ea19991d
Aryan Gupta <72204017+aryangupta02092002@users.noreply.github.com>
2023-05-27 12:05:12 +0530

    May 27 - Linked List

---
+++ GFG/ModifyLinkedList_1.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+struct Node* modifyTheList(struct Node *head);
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+void print(Node *head)
+{
+    Node *temp=head;
+	while (temp)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout<<endl;
+}
+
+
+// } Driver Code Ends
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+class Solution{
+    public:
+    struct Node* modifyTheList(struct Node *head)
+    {
+        //add code here.
+        vector<int> v;
+        Node* curr = head;
+        while(curr != NULL){
+            v.push_back(curr->data);
+            curr = curr->next;
+        }
+        int n = v.size();
+        int i = 0;
+        int j = n-1;
+        curr = head;
+        while(i < j){
+            curr->data = v[j] - v[i];
+            i++;
+            j--;
+            curr = curr->next;
+        }
+        while(curr != NULL){
+            curr->data = v[j];
+            curr = curr->next;
+            j--;
+        }
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--){
+		int n;
+		cin>>n;
+		struct Node *head = NULL,*temp=NULL;
+		while(n--){
+		    int a;
+            cin>>a;
+			Node *newNode = new Node(a);
+			if(head==NULL)
+                head=newNode;
+            else
+                temp->next=newNode;
+            temp=newNode;
+		}
+		Solution obj;
+		head = obj.modifyTheList(head);
+		print(head);
+		
+	}
+    return 0;
+}
+// } Driver Code Ends
